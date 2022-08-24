caeae96229380337fa72fd95120845e58d171738
sisby-folk <nora.herrah.heartstrings@gmail.com>
2022-08-24 13:03:23 +1000

    Working trinkets compat

--- src/main/java/folk/sisby/switchy_inventories/compat/TrinketsCompat.java
+++ src/main/java/folk/sisby/switchy_inventories/compat/TrinketsCompat.java
@@ -1,19 +1,72 @@
 package folk.sisby.switchy_inventories.compat;
 
-import dev.emi.trinkets.api.LivingEntityTrinketComponent;
+import dev.emi.trinkets.api.TrinketComponent;
 import dev.emi.trinkets.api.TrinketsApi;
+import dev.emi.trinkets.data.EntitySlotLoader;
+import folk.sisby.switchy.api.PresetModule;
 import folk.sisby.switchy.api.PresetModuleRegistry;
 import folk.sisby.switchy.api.modules.CardinalSerializerCompat;
 import folk.sisby.switchy_inventories.SwitchyInventories;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.util.Identifier;
 
-public class TrinketsCompat {
-	private static final Identifier ID = new Identifier(SwitchyInventories.ID,  "trinkets");
+public class TrinketsCompat implements PresetModule {
+	private static final Identifier ID = new Identifier(SwitchyInventories.ID, "trinkets");
+	private static final boolean isDefault = false;
+
+	private NbtCompound inventory = new NbtCompound();
+
+	@Override
+	public void updateFromPlayer(PlayerEntity player) {
+		this.inventory = new NbtCompound();
+		TrinketsApi.TRINKET_COMPONENT.get(player).writeToNbt(this.inventory);
+	}
+
+	@Override
+	public void applyToPlayer(PlayerEntity player) {
+		TrinketComponent component = TrinketsApi.TRINKET_COMPONENT.get(player);
+		// Recreate post-constructor state
+		component.getInventory().clear();
+		component.getGroups().clear();
+		component.update();
+		// Read from NBT
+		component.readFromNbt(inventory);
+		// Sync
+		EntitySlotLoader.INSTANCE.sync(player.getServer().getPlayerManager().getPlayerList());
+	}
+
+	@Override
+	public NbtCompound toNbt() {
+		return inventory;
+	}
+
+	@Override
+	public void fillFromNbt(NbtCompound nbt) {
+		this.inventory = nbt;
+	}
+
+	@Override
+	public Identifier getId() {
+		return ID;
+	}
+
+	@Override
+	public boolean isDefault() {
+		return isDefault;
+	}
+
+	@Override
+	public String getDisableConfirmation() {
+		return "WARNING: All items not currently present in your inventory will be IMMEDIATELY lost.";
+	}
+
 	public static void touch() {
+
 	}
 
 	// Runs on touch() - but only once.
 	static {
-		PresetModuleRegistry.registerModule(ID, () -> new CardinalSerializerCompat<>(TrinketsApi.TRINKET_COMPONENT, ID, () -> new LivingEntityTrinketComponent(null)));
+		PresetModuleRegistry.registerModule(ID, TrinketsCompat::new);
 	}
 }
