adab00066211c967c8232b4bdc95c12b965d7f17
sisby-folk <nora.herrah.heartstrings@gmail.com>
2023-09-01 15:57:57 +1000

    data trinkets

--- src/main/java/folk/sisby/switchy_inventories/SwitchyInventories.java
+++ src/main/java/folk/sisby/switchy_inventories/SwitchyInventories.java
@@ -1,20 +1,5 @@
 package folk.sisby.switchy_inventories;
 
-import folk.sisby.switchy.api.SwitchyEvents;
-import folk.sisby.switchy_inventories.modules.TrinketsCompat;
-import net.fabricmc.loader.api.FabricLoader;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-@SuppressWarnings("deprecation")
-public class SwitchyInventories implements SwitchyEvents.Init {
+public class SwitchyInventories {
 	public static final String ID = "switchy_inventories";
-	public static final Logger LOGGER = LoggerFactory.getLogger(ID);
-
-	@Override
-	public void onInitialize() {
-		if (FabricLoader.getInstance().isModLoaded("trinkets")) {
-			TrinketsCompat.touch();
-		}
-	}
 }
