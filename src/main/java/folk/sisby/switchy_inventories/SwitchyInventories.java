cc5ee709da4762c25329823fa7c071f0c71e753c
sisby-folk <nora.herrah.heartstrings@gmail.com>
2024-11-08 18:02:33 +1100

    modernize buildscripts

--- src/main/java/folk/sisby/switchy_inventories/SwitchyInventories.java
+++ src/main/java/folk/sisby/switchy_inventories/SwitchyInventories.java
@@ -6,7 +6,6 @@ import net.fabricmc.loader.api.FabricLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@SuppressWarnings("deprecation")
 public class SwitchyInventories implements SwitchyEvents.Init {
 	public static final String ID = "switchy_inventories";
 	public static final Logger LOGGER = LoggerFactory.getLogger(ID);
